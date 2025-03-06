ef151341fe443904d815e1efd1823e5417c1e860
fitia-fly <fitia.andriamamonjy@fly-technologies.com>
2025-03-06 12:53:11 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const sql = require("mssql");
 const app = express();
-const port = 8001;
+const port = process.env.PORT;
 const ldapjs = require("ldapjs");
 var DashboardRouter = require('./routes/dashboard')
 
